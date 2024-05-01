27045e5cdbfe40b868bc2a0eec640dae6e5e5d2c
Marie Rabel <marie.rabel0697@gmail.com>
2024-05-01 10:20:34 +0200

    Premiere partie day1

---
+++ vite-project/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import viteLogo from '/vite.svg'
+
+function Navbar () {
+    return (
+        <div className='navbar'>
+            <h1>To-Do App</h1>
+        <img src={viteLogo} alt="" />
+        </div>
+    )
+}
+
+export default Navbar
